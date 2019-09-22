58378b2758b0c3ab24b03a8e11ac1f92e632d115
kukeke11 <martinring@hotmail.com>
2019-09-22 15:04:22 +0300

    Add project files.

---
+++ Weather Project/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Weather_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_cityname = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbl_country = new System.Windows.Forms.Label();
+            this.lbl_temp = new System.Windows.Forms.Label();
+            this.lbl_main = new System.Windows.Forms.Label();
+            this.lbl_wind = new System.Windows.Forms.Label();
+            this.lbl_description = new System.Windows.Forms.Label();
+            this.lbl_humidity = new System.Windows.Forms.Label();
+            this.lbl_pressure = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_cityname
+            // 
+            this.lbl_cityname.AutoSize = true;
+            this.lbl_cityname.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cityname.Location = new System.Drawing.Point(77, 32);
+            this.lbl_cityname.Name = "lbl_cityname";
+            this.lbl_cityname.Size = new System.Drawing.Size(471, 73);
+            this.lbl_cityname.TabIndex = 0;
+            this.lbl_cityname.Text = "Tallinn, Estonia";
+            this.lbl_cityname.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Location = new System.Drawing.Point(21, 163);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(410, 292);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbl_country
+            // 
+            this.lbl_country.AutoSize = true;
+            this.lbl_country.Location = new System.Drawing.Point(107, 105);
+            this.lbl_country.Name = "lbl_country";
+            this.lbl_country.Size = new System.Drawing.Size(35, 13);
+            this.lbl_country.TabIndex = 3;
+            this.lbl_country.Text = "label1";
+            // 
+            // lbl_temp
+            // 
+            this.lbl_temp.AutoSize = true;
+            this.lbl_temp.Font = new System.Drawing.Font("Norwester", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_temp.Location = new System.Drawing.Point(479, 235);
+            this.lbl_temp.Name = "lbl_temp";
+            this.lbl_temp.Size = new System.Drawing.Size(136, 44);
+            this.lbl_temp.TabIndex = 4;
+            this.lbl_temp.Text = "label2";
+            // 
+            // lbl_main
+            // 
+            this.lbl_main.AutoSize = true;
+            this.lbl_main.Location = new System.Drawing.Point(554, 294);
+            this.lbl_main.Name = "lbl_main";
+            this.lbl_main.Size = new System.Drawing.Size(35, 13);
+            this.lbl_main.TabIndex = 5;
+            this.lbl_main.Text = "label1";
+            // 
+            // lbl_wind
+            // 
+            this.lbl_wind.AutoSize = true;
+            this.lbl_wind.Location = new System.Drawing.Point(554, 320);
+            this.lbl_wind.Name = "lbl_wind";
+            this.lbl_wind.Size = new System.Drawing.Size(35, 13);
+            this.lbl_wind.TabIndex = 6;
+            this.lbl_wind.Text = "label1";
+            // 
+            // lbl_description
+            // 
+            this.lbl_description.AutoSize = true;
+            this.lbl_description.Location = new System.Drawing.Point(554, 307);
+            this.lbl_description.Name = "lbl_description";
+            this.lbl_description.Size = new System.Drawing.Size(35, 13);
+            this.lbl_description.TabIndex = 7;
+            this.lbl_description.Text = "label1";
+            // 
+            // lbl_humidity
+            // 
+            this.lbl_humidity.AutoSize = true;
+            this.lbl_humidity.Location = new System.Drawing.Point(554, 333);
+            this.lbl_humidity.Name = "lbl_humidity";
+            this.lbl_humidity.Size = new System.Drawing.Size(35, 13);
+            this.lbl_humidity.TabIndex = 8;
+            this.lbl_humidity.Text = "label1";
+            // 
+            // lbl_pressure
+            // 
+            this.lbl_pressure.AutoSize = true;
+            this.lbl_pressure.Location = new System.Drawing.Point(554, 346);
+            this.lbl_pressure.Name = "lbl_pressure";
+            this.lbl_pressure.Size = new System.Drawing.Size(35, 13);
+            this.lbl_pressure.TabIndex = 9;
+            this.lbl_pressure.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Main";
+            this.label1.Click += new System.EventHandler(this.Label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Description";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Wind";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(488, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Humidity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(488, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Pressure";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(745, 577);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_pressure);
+            this.Controls.Add(this.lbl_humidity);
+            this.Controls.Add(this.lbl_description);
+            this.Controls.Add(this.lbl_wind);
+            this.Controls.Add(this.lbl_main);
+            this.Controls.Add(this.lbl_temp);
+            this.Controls.Add(this.lbl_country);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbl_cityname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_cityname;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbl_country;
+        private System.Windows.Forms.Label lbl_temp;
+        private System.Windows.Forms.Label lbl_main;
+        private System.Windows.Forms.Label lbl_wind;
+        private System.Windows.Forms.Label lbl_description;
+        private System.Windows.Forms.Label lbl_humidity;
+        private System.Windows.Forms.Label lbl_pressure;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Weather Project/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Weather_Project
+{
+    public partial class Form1 : Form
+    {
+        const string APPID = "3789c4e4e5b2d1a4633f2f89ef213e23";
+        string cityName = "Tallinn";
+
+        public Form1()
+        {
+            InitializeComponent();
+            getWeather(cityName, APPID);
+        }
+
+        void getWeather(string city, string id)
+        {
+            using (WebClient web = new WebClient())
+            {
+                var json = web.DownloadString(string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric&ctn=6", city, id));
+
+                var result = JsonConvert.DeserializeObject<WeatherGet.root>(json);
+                WeatherGet.root output = result;
+                lbl_cityname.Text = string.Format("{0}", output.name);
+                lbl_country.Text = string.Format("{0}", output.sys.country);
+                lbl_temp.Text = string.Format("{0} \u00b0"+"C", output.main.temp);
+                lbl_description.Text = string.Format("{0}", output.weather[0].description);
+                lbl_main.Text = string.Format("{0}", output.weather[0].main);
+                lbl_pressure.Text = string.Format("{0}"+" hPa", output.main.pressure);
+                lbl_humidity.Text = string.Format("{0}"+"%", output.main.humidity);
+                lbl_wind.Text = string.Format("{0}"+"m/s", output.wind.speed);
+                string ID = string.Format(output.weather[0].icon);
+                string imgpath =@"C:\Users\marti\source\repos\Weather Project\Weather Project\icons\";
+                string end = ".png";
+                pictureBox2.Image = Image.FromFile(imgpath+ID+end);
+            }
+        }
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
